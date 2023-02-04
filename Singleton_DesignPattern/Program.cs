6f988224c55db7eaf2d8158eeaeba2cea043e34e
shett <shetty.manasa10@gmail.com>
2023-02-04 17:25:59 -0500

    first-commit

---
+++ Singleton_DesignPattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_DesignPattern
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Teacher t1=Teacher.GetTeacher();
+            t1.Teach();
+
+            Console.WriteLine("===========================");
+
+            Teacher t2 = Teacher.GetTeacher();
+            t2.Teach();
+        }
+    }
+}
---
+++ Singleton_DesignPattern/Teacher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_DesignPattern
+{
+    public class Teacher
+    {
+        private static Teacher t1=new Teacher();
+        private Teacher()
+        {
+
+        }
+        public static Teacher GetTeacher()
+        {
+            return t1;
+        }
+
+        public void Teach()
+        {
+            Console.WriteLine("Teacher is teaching.....");
+        }
+    }
+}
